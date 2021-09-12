5b4a2d1dafb791c90255760a298c5dd62af92ba0
shantanu pandey <shantanupandey@shantanus-MacBook.local>
2021-09-12 16:57:16 -0400

    Added a controller and a router for home

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
 const app = express();
-
-
 const port = 8000;
+
+//Use express router
+
+app.use('/',require('./routes'));
 
 
 app.listen(port, function(err){
