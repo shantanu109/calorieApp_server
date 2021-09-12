c7529ca9b1a763acdef44dc61e62a893bc13c7da
shantanu pandey <shantanupandey@shantanus-MacBook.local>
2021-09-12 17:49:56 -0400

    Added static files and layout

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+
+
+app.use(expressLayouts);
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 //Use express router
 
 app.use('/',require('./routes'));
