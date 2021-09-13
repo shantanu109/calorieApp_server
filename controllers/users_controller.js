1e36ebfba1e911210f0b83e5f822bbbd00a5e0f4
shantanu pandey <shantanupandey@shantanus-MacBook.local>
2021-09-13 12:41:49 -0400

    Added the routes for Sign Up and Sign In

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,5 +2,18 @@ module.exports.profile = function(req,res){
     return res.render('user_profile', {
         title: 'User Profile'
     })
+}
 
+
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "CalorieApp | Sign Up"
+    })
+}
+
+
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "CalorieApp | Sign In"
+    })
 }
