8ad4ee698d9a9d7454e8d49f3378b4eb6a83f4cd
Urmi Pathak <53376580+urmi6899@users.noreply.github.com>
2021-09-30 15:11:46 -0400

    Update model.py

--- prediction_model/model.py
+++ prediction_model/model.py
@@ -5,16 +5,12 @@ from pymongo import MongoClient
 client = MongoClient()
 client = MongoClient('mongodb://localhost:27017')
 db = client['weight']
-df = db.usr_wt
+df = db.usr_weight
 df = pd.DataFrame(list(df.find()))
 
 date = df.iloc[:, 1]
 X = df.iloc[:, 2:-1]
 y = df.iloc[:, -1]
-
-normalize = MinMaxScaler()
-normalize.fit(X)
-X = normalize.transform(X)
 
 X = pd.DataFrame(X, columns=['calorie_intake', 'calorie_burn', 'surplus', 'weight', 'weight_change'])
 
